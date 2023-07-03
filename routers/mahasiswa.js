f0f735072fc954d86999d8dbcccb46059ee7396f
hadiqfawzie <hadiqfawzie@gmail.com>
2023-07-04 01:25:11 +0700

    initial commit

---
+++ routers/mahasiswa.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const routerMahasiswa = express.Router()
+
+const controllerMahasiswa = require('../controllers/mahasiswa')
+
+routerMahasiswa.route('/mahasiswa')
+    .post(controllerMahasiswa.insert)
+    .get(controllerMahasiswa.getMahasiswa)
+
+routerMahasiswa.route('/mahasiswa/:nim')
+    .get(controllerMahasiswa.getMahasiswaByNim)
+    .put(controllerMahasiswa.update)
+    .delete(controllerMahasiswa.delete)
+
+routerMahasiswa.route('/mahasiswa/nilai/:nim')
+    .get(controllerMahasiswa.getNilaiByNim)
+    .put(controllerMahasiswa.insertNilai)
+
+routerMahasiswa.route('/mahasiswa/nilai/:nim/:semester')
+    .get(controllerMahasiswa.getNilaiByNimSemester)
+
+    
+module.exports = routerMahasiswa
+
---
+++ routes.js
@@ -0,0 +1,3 @@
+export const get = get('/',(req,res) => {
+    res.send('get express');
+})
